ce77c17ece8fd5c5871d6424c4c2df378d17bbad
JAHJA Darwin <16094501d@msds.comp.polyu.edu.hk>
2018-11-29 18:29:55 +0800

    add categories

--- cs/eb-a2/mysql/table.sql
+++ cs/eb-a2/mysql/table.sql
@@ -4,3 +4,13 @@ CREATE TABLE IF NOT EXISTS `users` (
     `password` VARCHAR(255) NOT NULL,
     `level` TINYINT UNSIGNED NOT NULL
 ) ENGINE = InnoDB CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `menus` (
+    `food_id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `food_name` VARCHAR(100) NOT NULL,
+    `food_category` VARCHAR(100) NOT NULL,
+    `food_image_url` VARCHAR(255),
+    `food_description` VARCHAR(255) NOT NULL,
+    `food_price` INT(11) NOT NULL,
+    `food_available` TINYINT UNSIGNED NOT NULL
+) ENGINE = InnoDB CHARSET=utf8;
--- cs/eb-a2/src/adminMenuAdd.php
+++ cs/eb-a2/src/adminMenuAdd.php
@@ -11,11 +11,14 @@ if(array_key_exists('is_logged', $_SESSION) === FALSE && $_SESSION['level'] < 2)
 if ($_POST && isset($_POST['submit'])) {
     $new_menu = array(
         "food_name" => $_POST['food_name'],
+        "food_category" => $_POST['food_category'],
         "food_image_url" => $_POST['food_image_url'],
         "food_description" => $_POST['food_description'],
         "food_price" => $_POST['food_price'],
         "food_available"  => $_POST['food_available']
     );
+
+    print_r(array_values($new_menu));
 
     try {
         $sql = sprintf(
@@ -53,19 +56,26 @@ if ($_POST && isset($_POST['submit'])) {
     <form method="post" id="menu_add">
         <label for="food_name">Food Name</label>
         <input type="text" name="food_name" id="food_name" required>
+        <label for="food_category">Category</label>
+        <select name="food_category" required>
+            <option value="mains">Mains</option>
+            <option value="drinks">Drinks</option>
+            <option value="others">Others</option>
+        </select>
         <label for="food_image_url">Image URL</label>
         <input type="url" name="food_image_url" id="food_image_url">
         <label for="food_description">Description</label>
         <textarea name="food_description" id="food_description" form="menu_add" cols="30" rows="10" required></textarea>
-        <label for="food_price">Price</label>
+        <label for="food_price">Price (HK$)</label>
         <input type="number" name="food_price" id="food_price" required>
 
         <label for="food_available">Availability</label>
         <input type="radio" name="food_available" value="1" required>Available<br>
         <input type="radio" name="food_available" value="0">Out of Order<br>
-
+        <br>
         <input class="but" type="submit" name="submit" value="Submit">
     </form>
+    <br>
     <a href="adminMenuManager.php">Back to menu manager</a>
 </body>
 </html>
